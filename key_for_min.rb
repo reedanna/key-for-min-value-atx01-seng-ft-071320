e13ecd479ffbfcc7a8fc0ddac3605711c9941db0
Anna Nicole Reed <reedanna@mail.com>
2020-06-26 20:11:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  minimum = ""
+  answer = nil
+  name_hash.each do |key, value|
+    if value < minimum || minimum == ""
+      minimum = value
+      answer = key
+  end
+  answer
 end
